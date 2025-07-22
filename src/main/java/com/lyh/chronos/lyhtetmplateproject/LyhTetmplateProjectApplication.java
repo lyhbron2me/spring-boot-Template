abe7111b3b933c9bbdf8c1665566a92e7f0f194b
lyhbron2me <2396914638@qq.com>
2025-07-22 16:53:10 +0800

    build:重命名配置文件并添加数据源及日志配置- 将 application.properties 重命名为 application.yml - 在 application.yml 中添加服务器端口配置 - 添加 MySQL 数据源配置 - 添加 MyBatis-Plus 日志配置- 移除 LyhTetmplateProjectApplication 中的注释

--- src/main/java/com/lyh/chronos/lyhtetmplateproject/LyhTetmplateProjectApplication.java
+++ src/main/java/com/lyh/chronos/lyhtetmplateproject/LyhTetmplateProjectApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+// (exclude = {DataSourceAutoConfiguration.class}) TODO 启动时排除数据源配置，因为项目没有数据源
 @SpringBootApplication
 public class LyhTetmplateProjectApplication {
 
