cd50e78680b65a560fd8feea244e4ad185be8924
lyhbron2me <2396914638@qq.com>
2025-07-22 14:30:39 +0800

    feat: 创建基础的 Spring Boot项目结构
    - 添加 Maven 项目对象模型文件 (pom.xml)- 创建项目目录结构，包括源代码、资源文件和测试文件- 添加基础的 Spring Boot 启动类和测试类
    -配置 Spring Boot 应用属性
    - 添加 .gitattributes 和 .gitignore 文件，规范代码仓库管理
    - 添加 Maven Wrapper 配置文件和脚本

---
+++ src/main/java/com/lyh/chronos/lyhtetmplateproject/LyhTetmplateProjectApplication.java
@@ -0,0 +1,14 @@
+package com.lyh.chronos.lyhtetmplateproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class LyhTetmplateProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LyhTetmplateProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lyh/chronos/lyhtetmplateproject/LyhTetmplateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyh.chronos.lyhtetmplateproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyhTetmplateProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
